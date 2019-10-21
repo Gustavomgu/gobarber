a6d6d219dedfd20fed2724abe9631b72205173d2
Gustavo <Gustavomgu>
2019-10-21 10:55:23 -0300

    chore: Implementing validations with yup

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,11 +1,24 @@
 import jwt from 'jsonwebtoken';
 
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 import auth from '../../config/auth';
 
 class SessiosController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails!' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
