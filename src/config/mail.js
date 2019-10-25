49a34ba4ebe6466bcbb4da2b940c238e1cdf8d50
Gustavo <Gustavomgu>
2019-10-25 09:52:37 -0300

    chore: Send email with nodemailer and mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '65e761be089cfa',
+    pass: '33ec8126b9bf46',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
