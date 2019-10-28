a268dc95a089870bb6b30915326b3351d7103272
Gustavo <Gustavomgu>
2019-10-28 12:08:04 -0300

    chore: Add envoriment variables and Sentry.io

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
+import 'dotenv/config';
 // Só é possivel usar a sintaxe import por usar a biblioteca sucrase
 import express from 'express';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
 import path from 'path';
+
+// Tem que ser importado antes das rotas pois vai captar erros das rotas
+import 'express-async-errors';
 import routes from './routes';
+import ConfigSentry from './config/sentry';
 
 import './database/index';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(ConfigSentry);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +34,20 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  // Middlewares com quatro parametros são entendidos automaticamente como tratamento de exceção pelo express
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn: process.env.SENTRY_DSN,
+  // 'https://31a351c6200b4bbe8f0d58f193581960@sentry.io/1798701',
+};
