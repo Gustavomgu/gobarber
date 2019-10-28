b385a6b654bdfec101ae5eda16ada0d05e0636cc
Gustavo <Gustavomgu>
2019-10-28 12:17:12 -0300

    chore: Implementing Env variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '65e761be089cfa',
-    pass: '33ec8126b9bf46',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobarber.com>',
